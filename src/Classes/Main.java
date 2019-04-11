334602a0f9ce9be15eb928122e5e81391d8aa7de
alingasada_sd2021 <alingasada_sd2021@448-R1-PC5.ceaccs.net>
2019-04-11 17:43:13 +0800

    First commit

--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -15,14 +15,6 @@ public class Main {
 
    public static void main(String args[]){
        
-//       Server server = new Server();
-//       server.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//       server.setLocationRelativeTo(null);
-//       server.runServer();
-//
-//       Client client = new Client("172.16.8.36");
-//       client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//       client.setLocationRelativeTo(null);
-//       client.runClient();
+   Controller cont = new Controller();
    }
 }
