8ce2a0776787cbc7f89cb0cb5df7c99d17622413
alingasada_sd2021 <alingasada_sd2021@448-R1-PC5.ceaccs.net>
2019-04-11 14:50:59 +0800

    First commit

---
+++ src/Classes/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author alingasada_sd2021
+ */
+public class Main {
+
+   public static void main(String args[]){
+       Server server = new Server();
+       server.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       server.setLocationRelativeTo(null);
+       server.runServer();
+   }
+}
