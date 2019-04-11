82800bf838ebf420277a7e434ba3ea1605b57c90
alingasada_sd2021 <alingasada_sd2021@448-R1-PC5.ceaccs.net>
2019-04-11 16:39:57 +0800

    First commit

--- src/Classes/TicTacToe.java
+++ src/Classes/TicTacToe.java
@@ -5,14 +5,27 @@
  */
 package Classes;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author alingasada_sd2021
  */
 public class TicTacToe {
-   
+    
+    private ArrayList<Integer> availList;
+    
     public TicTacToe() {
-        
+        this.availList = new ArrayList();
+        for(int i = 0;i<9;i++){
+            availList.add(i+1);
+        }
     }
+
+    public ArrayList<Integer> getAvailList() {
+        return availList;
+    }
+    
+    
     
 }
