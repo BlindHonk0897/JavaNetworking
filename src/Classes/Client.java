f93d22abd15e7c85242f180e237335ab2bd35c50
alingasada_sd2021 <alingasada_sd2021@448-R1-PC5.ceaccs.net>
2019-04-11 15:38:19 +0800

    First commit

--- src/Classes/Client.java
+++ src/Classes/Client.java
@@ -148,5 +148,12 @@ public class Client extends JFrame{
             }
         });
     }
-    
+    public static void main(String args[]){
+       
+
+       Client client = new Client("127.0.0.1");
+       client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       client.setLocationRelativeTo(null);
+       client.runClient();
+   }
 }
